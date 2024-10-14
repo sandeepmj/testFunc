20ae9828d6d1406637a1929625294c7c50fc1cc0
Sandeep Junnarkar <sjnews@gmail.com>
2024-10-14 16:19:27 -0400

    added timer

--- mathFunctions/__init__.py
+++ mathFunctions/__init__.py
@@ -1,2 +1,2 @@
 ## .coolCalcs refers to file that holds all the functions
-from .coolCalcs import addNumbers, subtractNumbers, gatherFiles
+from .coolCalcs import addNumbers, subtractNumbers, gatherFiles, timer
--- mathFunctions/coolCalcs.py
+++ mathFunctions/coolCalcs.py
@@ -22,3 +22,17 @@ def gatherFiles(path):
     returns all target files in list
     '''
     return glob.glob(path)
+
+
+import time
+from random import randrange
+
+def timer(start_time, end_time):
+    '''
+    Timer to snooze while scraping
+    Para_1: start_time: integer for minimum seconds snooze
+    Para_2: end_time: integer for maximum seconds snooze
+    '''
+    snoozer = randrange(start_time, end_time)
+    print(f"Snoozing for {snoozer} seconds!")
+    time.sleep(snoozer)## set timer
